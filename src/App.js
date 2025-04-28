9c61816cce0cecbf75b4e34e5f7b0b491ac2e561
Vimalraj J <vimalraj.j@tech9.com>
2025-04-28 22:38:29 +0530

    Added logo and search box UI implemented  - Added smooth animation for transition of search box  - Added gradient color for background

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [isNewSearch, setIsNewSearch] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState({
     items: [],
     pageInfo: null,
@@ -22,6 +23,7 @@ const App = () => {
         key: process.env.REACT_APP_API_KEY,
       };
       const qs = new URLSearchParams(params);
+      setLoading(true);
       const response = await fetch(`https://www.googleapis.com/youtube/v3/search?${qs.toString()}`);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -30,6 +32,8 @@ const App = () => {
       setData({ items, pageInfo, nextPageToken })
     } catch(error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -42,10 +46,14 @@ const App = () => {
 
   return (
     <div className='youtube-search-component'>
-      <form onSubmit={handleSearch}>
-        <input type='text' name='searchBox' value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
-      </form>
-      <div className='card-container'>
+      <div className='header'>
+        <img src="./logo.png" alt="logo" className='brand-logo'/>
+        <form onSubmit={handleSearch}>
+          <input type='text' name='searchBox' className={`search-box ${data.items.length ? 'top' : 'center'}`} placeholder='Search ...'
+            value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+        </form>
+      </div>
+      <div className='card-container list'>
         {data.items?.map(item => <VideoCard item={item} />)}
       </div>
 
